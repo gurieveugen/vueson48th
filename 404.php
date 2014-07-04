6d7a57fa6b3951e6dc967532af8bba0740ab100d
gurieveugen <gurievinvest@gmail.com>
2014-07-04 11:56:29 +0300

    0.010

--- 404.php
+++ 404.php
@@ -1,25 +1,25 @@
-<?php
-/**
- * The template for displaying 404 pages (Not Found).
- *
- * @package WordPress
- * @subpackage Twenty_Thirteen
- * @since Twenty Thirteen 1.0
- */
-
-get_header(); ?>
-
-		<article class="page-post site-content cf">
-		  <header class="tit-page">
-			  <h1><?php _e( 'Not found', 'twentythirteen' ); ?></h1>
-			</header>
-
-			<div class="entry">
-			  <h2><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentythirteen' ); ?></h2>
-				<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentythirteen' ); ?></p>
-
-				<?php get_search_form(); ?>
-			</div>
-		</article><!-- #post -->
-		
+<?php
+/**
+ * The template for displaying 404 pages (Not Found).
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+
+get_header(); ?>
+
+		<article class="page-post site-content cf">
+		  <header class="tit-page">
+			  <h1><?php _e( 'Not found', 'twentythirteen' ); ?></h1>
+			</header>
+
+			<div class="entry">
+			  <h2><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentythirteen' ); ?></h2>
+				<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentythirteen' ); ?></p>
+
+				<?php get_search_form(); ?>
+			</div>
+		</article><!-- #post -->
+		
 <?php get_footer(); ?>
---
+++ contact-page.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template Name: Contact Page
+ */
+$options = $GLOBALS['theme_settings']->getAll();
+extract($options);
+get_header(); ?>
+<?php /* The loop */ ?>
+<?php while ( have_posts() ) : the_post(); ?>
+<article class="contact-post">
+  <header class="tit-page">
+	  <h1><?php the_title(); ?></h1>
+	</header>
+
+	<div class="entry">
+	  <?php the_content(); ?>
+		<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
+	</div>
+
+</article>
+<?php endwhile; ?>
+<?php get_footer(); ?>
